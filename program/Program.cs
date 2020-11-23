88a784e32cf81c68d9abf00312ed205faf5b4c3d
unknown <Alamin>
2020-11-23 22:44:04 +0600

    final lab 1

---
+++ program/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a2 = new SavingsAccount("Alamin", "3333", 30000);
+            a2.transaction(1500);
+            Console.WriteLine();
+            a2.transaction(2000);
+            a2.transaction(2000);
+            a2.transaction(2000);
+            a2.transaction(2000);
+            a2.transaction(2000);
+
+            a2.transaction(2000);
+            a2.ShowInfo();
+            Console.WriteLine();
+
+
+            FixedAccount a4 = new FixedAccount("Rana ", "4444", 5555, 20000);
+            a4.transaction(3000);
+            a4.ShowInfo();
+            Console.WriteLine();
+
+            SpecialSavingsAccount a1 = new SpecialSavingsAccount("Ahamed", "6666", 2000);
+            SpecialSavingsAccount a7 = new SpecialSavingsAccount("Ahamed", "7777", 1000);
+
+            a1.transaction(300);
+            a1.transaction(300);
+            a1.transaction(300);
+            a1.transaction(300);
+            a1.transaction(300);
+            a1.transaction(300); 
+
+            a7.transaction(500);
+            a1.ShowInfo();
+            a7.ShowInfo();
+            Console.WriteLine();
+
+
+            Account a3 = new OverDraftAccount("Ahamed ", "111", 10000); 
+            a3.transaction(5000);
+            a3.ShowInfo();
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ program/savingaccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_lab_1
+{
+    class savingsaccount : Account
+    {
+        int trans = 0;
+
+        public savingsaccount(string name, string id, double balance) : base(name, id, balance)
+        {
+
+        }
+
+        override public void transaction(double amount)
+        {
+
+            if (trans >= 5)
+            {
+                Console.WriteLine("Sorry The maximum transaction time commited");
+            }
+            else if (this.balance - amount < 5000)
+            {
+                Console.WriteLine("Sorry ! transaction couldn't be completed due to the balance less than 5000$");
+            }
+            else
+            {
+                this.balance = this.balance - amount;
+                trans++;
+            }
+
+        }
+        override public void ShowInfo()
+        {
+            Console.WriteLine("Name :" + name + " ID :" + id + "  Balance :" + balance);
+        }
+    }
+}
